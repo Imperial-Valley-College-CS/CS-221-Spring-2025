217de858113218d3b2e0a688aa0340e685835478
ortoct <104336410+ortoct@users.noreply.github.com>
2025-02-24 14:33:41 -0800

    Updated ValidEmail

---
+++ Fundamentals/PA4_ValidEmail.java
@@ -0,0 +1,18 @@
+public class PA4_ValidEmail
+{
+   public static void main(String[] args)
+   {
+      //scan from user: name and email
+      String name = "Olivia Ortiz";
+      String email = "o.ortiz@students.imperial.edu";
+      
+      int space = name.indexOf(' ');   //index value of space
+      char firstI = name.toLowerCase().charAt(0);    //character at index value 0
+      String lastName = name.toLowerCase().substring(space+1);   //grab last name
+      String domain = "@students.imperial.edu";
+      
+      String expEmail = firstI+"."+lastName+domain;
+      
+      boolean valid = email.equals(expEmail);
+   }
+}
