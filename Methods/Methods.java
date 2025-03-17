fc29ed7777cc2118e2bc7fa783cc7d878aa74337
ortoct <104336410+ortoct@users.noreply.github.com>
2025-03-17 14:12:58 -0700

    Overloaded getGP

--- Methods/Methods.java
+++ Methods/Methods.java
@@ -4,8 +4,10 @@ public class Methods
    {
       double ans1 = gpa('A', 'B', 'b', 'a');      //invoke(call) gpa
       double ans2 = gpa('C', 'c', 'A', 'B', 'A');      //invoke(call) gpa
+      double ans3 = gpa(95, 82, 77, 96);      //invoke(call) gpa
       System.out.println( ans1 );
       System.out.println( ans2 );
+      System.out.println( ans3 );
    }//end main
    
    public static double gpa(char gr1, char gr2, char gr3, char gr4, char gr5)
@@ -33,6 +35,18 @@ public class Methods
       return someVar/4.0;    //placeholder for now
    }//end gpa
    
+   public static double gpa(int gr1, int gr2, int gr3, int gr4)
+   {
+      double someVar = 0.0;
+      
+      someVar += getGP(gr1);
+      someVar += getGP(gr2);
+      someVar += getGP(gr3);
+      someVar += getGP(gr4);      
+      
+      return someVar/4.0;    //placeholder for now
+   }//end gpa
+   
    public static double getGP( char gr )
    {
       if( gr == 'A' || gr == 'a' )
@@ -45,6 +59,20 @@ public class Methods
          return 1.0;
          
       return 0.0;
+   }//end getGP
+   
+   public static double getGP( int gr )
+   {
+      if( gr >= 90 )
+         return 4.0;
+      else if( gr >= 80 )
+         return 3.0;
+      else if( gr >= 70 )
+         return 2.0;
+      else if( gr >= 60 )
+         return 1.0;
+      else   
+         return 0.0;
    }//end getGP
    
 }//end class
