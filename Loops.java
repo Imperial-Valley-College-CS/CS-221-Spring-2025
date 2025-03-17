2605c29521b1dd69ce5a647ff5a9a737c9879269
ortoct <104336410+ortoct@users.noreply.github.com>
2025-03-17 13:37:30 -0700

    Updated methods

--- Loops.java
+++ Loops.java
@@ -4,12 +4,15 @@ public class Loops
    {
       String myName = "Bruce Wayne";
       int numVowels = 0;
+      
       for(int i = 0; i < myName.length(); i++)
       {
          char let = myName.toLowerCase().charAt(i);
-         if( let == 'a' || let == 'e' || let == 'i' || let == 'o' || let == 'u' )
-            numVowels++;
-      }
+         if( let == 'a' || let == 'e' || let == 'i' || let == 'o' || let == 'u' ) 
+         { 
+            numVowels++; 
+         }//end if
+      }//end loop
       System.out.println( numVowels );
-   }
-}
+   }//end main
+}//end class
---
+++ Methods/Methods.java
@@ -0,0 +1,17 @@
+public class Methods
+{
+   public static void main(String[] args)
+   {
+      double ans1 = gpa('A', 'B', 'b', 'a');      //invoke(call) gpa
+      double ans2 = gpa('C', 'c', 'A', 'B');      //invoke(call) gpa
+      System.out.println( ans1 );
+      System.out.println( ans2 );
+   }//end main
+   
+   public static double gpa(char gr1, char gr2, char gr3, char gr4)
+   {
+      double someVar = 0.0;
+      return someVar;    //placeholder for now
+   }//end gpa
+   
+}//end class
