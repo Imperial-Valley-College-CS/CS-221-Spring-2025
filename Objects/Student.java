f532f0a168e35c6efb28a1cd9913db2d314c2216
ortoct <104336410+ortoct@users.noreply.github.com>
2025-04-07 14:25:29 -0700

    Student constructor

--- Objects/Student.java
+++ Objects/Student.java
@@ -1,7 +1,18 @@
+import java.util.GregorianCalendar;
+
 public class Student
 {
+   //Data fields (characteristics)
+   private String name;
+   private GregorianCalendar dob;
+   
    //Constructor(s)
-   public Student(String n)
+   public Student(String n, String dateOfBirth)
    {
+      this.name = n;
    }
+   
+   //method (getter or setter or specialized)
+   public String getName(){ return this.name; }
+   
 }
--- Objects/TestStudent.java
+++ Objects/TestStudent.java
@@ -3,6 +3,7 @@ public class TestStudent
    public static void main(String[] args)
    {
       //Create Student object (instance of a Student)
-      Student s = new Student("Joe Lopez");
+      Student s = new Student("Joe Lopez", "10/12/2003");
+      System.out.println( s.getName() );
    }
 }
