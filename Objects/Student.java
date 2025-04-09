d92931c43df9f0b7df946ee52758f560cc54dea1
ortoct <104336410+ortoct@users.noreply.github.com>
2025-04-09 13:42:12 -0700

    Updated Student

--- Objects/Student.java
+++ Objects/Student.java
@@ -3,13 +3,21 @@ import java.util.GregorianCalendar;
 public class Student
 {
    //Data fields (characteristics)
-   private String name;
+   private String name, gender;
    private GregorianCalendar dob;
+   private double gpa;
+   private int age;
    
    //Constructor(s)
-   public Student(String n, String dateOfBirth)
+   public Student(String n, String dateOfBirth, String g)
    {
       this.name = n;
+      this.gender = g;
+      setDOB();
+   }
+   
+   private void setDOB()
+   {
    }
    
    //method (getter or setter or specialized)
--- Objects/TestStudent.java
+++ Objects/TestStudent.java
@@ -3,7 +3,17 @@ public class TestStudent
    public static void main(String[] args)
    {
       //Create Student object (instance of a Student)
-      Student s = new Student("Joe Lopez", "10/12/2003");
-      System.out.println( s.getName() );
+      Student sto = new Student("Joe Lopez","10/12/2003","male");
+      Student stu = new Student("Lisa Lopez", "6/1/2005","female");
+      
+      Student[] roster = new Student[2];
+      roster[0] = sto;
+      roster[1] = stu;
+      
+      for( Student x : roster )
+      {
+         //prints what toString method returns
+         System.out.println( x );      
+      }
    }
 }
