6af4ef15bb78dd7490ae1f86657a02a9af0dda74
ortoct <104336410+ortoct@users.noreply.github.com>
2025-04-28 14:27:20 -0700

    Updated Student class

--- Inheritance/Student.java
+++ Inheritance/Student.java
@@ -3,11 +3,25 @@ public class Student extends Person
    //data fields
    private double gpa;
    private String major;
+   private char[] grades;
    
    //constructor
    public Student(String name, String gender, String dob, String major)
    {
       super(name, gender, dob); //invoke the constructor of superclass
       this.major = major;
+   }
+   
+   public void setGrades( char[] g )
+   {
+      this.grades = g;
+      calcGPA();
+   }
+   
+   private void calcGPA()
+   {
+      double gp = 0.0;
+      //missing code
+      this.gpa = gp/this.grades.length;
    }
 }
