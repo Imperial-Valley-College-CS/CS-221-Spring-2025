b9e04c0d6a3d4d6bbb4d0c82df099f50e976d603
ortoct <104336410+ortoct@users.noreply.github.com>
2025-05-21 14:14:28 -0700

    Added AnimationTimer

--- JavaFX/Snake/App.java
+++ JavaFX/Snake/App.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.canvas.*;
+import javafx.animation.AnimationTimer;
 
 public class App extends Application
 {
@@ -9,12 +10,35 @@ public class App extends Application
    GraphicsContext gc = canvas.getGraphicsContext2D();
    Group g = new Group(canvas);                 //is a Parent
    Scene scene = new Scene(g, Constants.WIDTH, Constants.HEIGHT);  //needs a Parent
+   Timer t = new Timer();
+   int x = 0;
+   int y = 50;
+   
    @Override
    public void start(Stage s)
    {
-      gc.setFill(Constants.COLOR_APPLE);
-      gc.fillRect(10,50,20,20);
+      t.start();        //starts Timer (handle method is invoked on every frame)
       s.setScene(scene);      //stage needs a Scene
       s.show();
-   }
-}
+   }//end start
+   
+   public void drawSquare()
+   {
+      gc.setFill(Constants.COLOR_CANVAS);
+      gc.fillRect(0,0, Constants.WIDTH, Constants.HEIGHT);
+      
+      gc.setFill(Constants.COLOR_APPLE);
+      gc.fillRect(x,y,20,20);   
+      x += 10; 
+   }//end drawSquare
+   
+   class Timer extends AnimationTimer
+   {
+      @Override
+      public void handle( long now )
+      {
+         drawSquare();
+      }
+   }//end Timer
+   
+}//end App
